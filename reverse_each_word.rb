a876f7cf1e731c7d3a240d0b4ae900b4029dffed
Cissica Gonzalez Martinez <cecemartinez@hotmail.com>
2020-08-07 06:46:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,15 +1,5 @@
 
 
-def reverse_each_word(string)
-  new_array = []
-  addl_array = []
-  new_array = string.split
-  new_array.each do |word|
-    addl_array << word.reverse
-  end
-return addl_array.join(" ")
-third_array
-end
 def reverse_each_word_collect(string)
   sen = sentence1.split(" ")
   new_arr = sen.collect {|a| a.reverse}
